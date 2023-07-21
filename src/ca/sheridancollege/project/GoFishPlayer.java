8b639353daf85f668402781efa9c89d1c747f851
sohalharman <sohaharm@sheridancollege.ca>
2023-07-21 05:16:06 -0400

    This is the 6th commit made by Harmandeep Singh Sohal on 21 July, 2023 at 5:15 am. I added a GoFishPlayer class to extend the Player class

---
+++ src/ca/sheridancollege/project/GoFishPlayer.java
@@ -0,0 +1,14 @@
+package ca.sheridancollege.project;
+
+/**
+ * This class +++ Insert class description here +++
+ * @author Harmandeep Singh Sohal
+ */
+public class GoFishPlayer extends Player{
+    public GoFishPlayer(String name){
+        super(name);
+    }
+    @Override
+    public void play(){
+    }
+}
